3d32912d77853b1393fa8f70180353c0ccd6b7a9
080599 <47007851+080599@users.noreply.github.com>
2024-06-27 12:57:28 +0500

    new

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import CategeryModel
-admin.site.register(CategeryModel)
+from .models import CategoryModel,ProductModel,Cart
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    search_fields =['category_name']
+    list_display=['id','category_name','created_at']
+    list_filter = ["created_at"]
+    ordering = ['id']
+@admin.register(ProductModel)
+class ProductModelAdmin(admin.ModelAdmin):
+    search_fields=['product_mname']
+    list_dispaly=['id','product_name','price','count','created_at']
+    list_filter=['created_at']
+    ordering=['-id']
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    search_fields=['product_mname']
+    list_dispaly=['id','product_name','price','count','created_at']
+    list_filter=['created_at']
+    ordering=['-id']
+#admin.site.register(CategoryModel)
 # Register your models here.
--- products/models.py
+++ products/models.py
@@ -1,4 +1,46 @@
 from django.db import models
-class CategeryModel(models.Model):
-    category_name=models.CharField(max_length=50)
-# Create your models here.
+
+
+class CategoryModel(models.Model):
+    category_name = models.CharField(max_length=50)
+    # Create your models here.
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    def __str__(self):
+        return self.category_name
+    class Meta:
+        verbose_name="Category"
+        verbose_name_plural="Categories"
+class ProductModel(models.Model):
+    product_name=models.CharField(max_length=80)
+    price=models.FloatField()
+    category=models.ForeignKey(CategoryModel,on_delete=models.CASCADE,null=True)
+    count=models.IntegerField(default=0)
+    descriptions=models.TextField()
+    image=models.FileField(upload_to='product_image')
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.product_name
+    class Meta:
+        verbose_name="Product"
+        verbose_name_plural="Products"
+class Cart(models.Model):
+    product_name=models.CharField(max_length=80)
+    price=models.FloatField()
+    category=models.ForeignKey(ProductModel,on_delete=models.CASCADE,null=True)
+    count=models.IntegerField(default=0)
+    image=models.FileField(upload_to='product_image')
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.product_name
+    class Meta:
+        verbose_name="Cart"
+
+class NewsCategory(models.Model):
+    name = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "NewsCategory"
